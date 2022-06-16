dfab31f623827fd240c1eb8f2e4807140c584c4d
ueuuop8591 <leo4707@gmail.com>
2022-06-16 14:40:48 +0800

    calendar

--- frontend/src/component/column/column.js
+++ frontend/src/component/column/column.js
@@ -47,11 +47,11 @@ export const columns4 = [
     { field: 'stockName', headerName: '股票名稱', flex: 1, headerAlign: 'center', align: 'center' },
     { field: 'Date', headerName: '法說會日期', flex: 1, headerAlign: 'center', align: 'center' },
     { field: 'Time', headerName: '法說會時間', flex: 1, headerAlign: 'center', align: 'center' },
-    { field: 'Form', headerName: '法說會形式', flex: 1, headerAlign: 'center', sortable: false, align: 'center', renderCell : rowData => (check_Form_information(rowData.value)) },
+    { field: 'Form', headerName: '法說會形式', flex: 1, headerAlign: 'center', sortable: false, align: 'center' },
     { field: 'Message', headerName: '法說會訊息', flex: 1, headerAlign: 'center', sortable: false, align: 'center' },
     { field: 'chPDF', headerName: '中文檔案', flex: 1, headerAlign: 'center', sortable: false, align: 'center', renderCell : rowData => <a href = { rootApiIP + "/data/download/single_twse_chPDF_download?filename=" + rowData.value } download = { rowData.value}>Download</a> },
     { field: 'enPDF', headerName: '英文檔案', flex: 1, headerAlign: 'center', sortable: false, align: 'center', renderCell : rowData => <a href = { rootApiIP + "/data/download/single_twse_enPDF_download?filename=" + rowData.value } download = { rowData.value}>Download</a> },
-    { field: 'More information', headerName: '相關資訊', flex: 1, headerAlign: 'center', sortable: false, align: 'center', renderCell : rowData => (check_Form_information(rowData.value)) },
+    { field: 'More information', headerName: '相關資訊', flex: 1, headerAlign: 'center', sortable: false, align: 'center' },
     { field: 'Video address', headerName: '影音連結資訊', flex: 1, headerAlign: 'center', sortable: false, align: 'center' },
     { field: 'Attention', headerName: '其他應敘明事項', flex: 1, headerAlign: 'center', sortable: false, align: 'center'},
 ];
@@ -69,13 +69,5 @@ const check_single_lineMemo_memo_NULL = (value) => {
         return <> </>
     }else{
         return <CustomA value = { rootApiIP + "/data/download/single_line_memo?filename=" + value } />
-    }
-}
-
-const check_Form_information = (value) => {
-    if(value.includes("http")){
-        return <a href = {value} target = "_blank" rel = "noopener noreferrer">點我</a>
-    }else{
-        return <span>{value}</span>
     }
 }

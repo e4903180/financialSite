00e112de13ca9e03cb7ace55f87352b6086696f9
ueuuop8591 <leo4707@gmail.com>
2023-02-15 18:11:05 +0800

    frontend constant change

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -14,8 +14,6 @@ const prod = {
 
 export const config = process.env.NODE_ENV === "development" ? dev : prod
 
-// export const rootApiIP = "http://140.116.214.154:3000/api"
-// export const WebSocketIP = "http://140.116.214.154:3000?username="
 export const WSContext = createContext()
 
 export const baseAna = [{method: "股票定價策略", url : "/stock_pricing_stratagy"}, {method: "本益比河流圖", url : "/PER_River"}]

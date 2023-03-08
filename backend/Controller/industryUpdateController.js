61171a01304818533b96db7afad27b829726150b
ueuuop8591 <leo4707@gmail.com>
2023-03-08 18:20:56 +0800

    industry_search post to get

--- backend/Controller/industryUpdateController.js
+++ backend/Controller/industryUpdateController.js
@@ -45,6 +45,7 @@ exports.industry_upload = async function(req, res){
 
         return res.status(200).send("success");
     } catch (error) {
+
         return res.status(400).send("error")
     }
 };

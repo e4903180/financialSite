d51ce9a7763539f9170041793f2ab117a322eaa3
ueuuop8591 <leo4707@gmail.com>
2023-03-16 18:13:11 +0800

    gmail handle

--- DB/gmail/gmailResearchHandle.py
+++ DB/gmail/gmailResearchHandle.py
@@ -112,18 +112,21 @@ class Pattern():
             return result
 
         elif "元富投顧" in info["subject"]:
-            idx_left_brackets = info["subject"].find(stock_nums[0] + ")")
-            
-            idx_end = idx_left_brackets + 6
+            for stock_num in stock_nums:
+                idx_left_brackets = info["subject"].find(stock_num + ")")
+                
+                idx_end = idx_left_brackets + 6
 
-            # Fwd: 元富投顧個股報告--大成鋼(2027)維持買進，最壞狀況已過，客戶拉貨回穩
-            # Fwd: 元富投顧訪談速報--旭富(4119)中立轉買進
-            while ((idx_end != len(info["subject"])) and 
-                (info["subject"][idx_end] != "，")):
-                idx_end += 1
+                # Fwd: 元富投顧0316每日股市彙報--世芯-KY(3661)維持買進、群聯(8299)維持買進、櫻花(9911)維持買進、華夏(1305)維持買進
+                # Fwd: 元富投顧個股報告--大成鋼(2027)維持買進，最壞狀況已過，客戶拉貨回穩
+                # Fwd: 元富投顧訪談速報--旭富(4119)中立轉買進
+                while ((idx_end != len(info["subject"])) and 
+                    (info["subject"][idx_end] != "，") and 
+                    (info["subject"][idx_end] != "、")):
+                    idx_end += 1
 
-            temp = info["subject"][idx_left_brackets + 5:idx_end].replace("/", "")
-            result.append(temp)
+                temp = info["subject"][idx_left_brackets + 5:idx_end].replace("/", "")
+                result.append(temp)
 
             return result
 

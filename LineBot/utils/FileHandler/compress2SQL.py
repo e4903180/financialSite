09346908fd7dfa0b73b8cfc6ba9ccb2e213f5296
ueuuop8591 <leo4707@gmail.com>
2023-04-05 15:53:24 +0800

    linebot compress to sql change

--- LineBot/utils/FileHandler/compress2SQL.py
+++ LineBot/utils/FileHandler/compress2SQL.py
@@ -34,6 +34,7 @@ class Compress2SQL():
 
         for decompress_file in decompress_files:
             field = decompress_file.split("_")
+            field[-1] = field[-1][:-4]
 
             key = self._find_key(field[0])
 

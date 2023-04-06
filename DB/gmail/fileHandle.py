bec534f3e4a78a295f264880d5b2c9aa3132da25
ueuuop8591 <leo4707@gmail.com>
2023-04-06 15:27:17 +0800

    gmail change

--- DB/gmail/fileHandle.py
+++ DB/gmail/fileHandle.py
@@ -434,7 +434,7 @@ class FileHandle():
             Return :
                 None
         """
-        dir_path = f"{self._unhandle_path}/2"
+        dir_path = f"{root_path['UNZIP_PATH']}/{datetime.datetime.now().strftime('%Y%m%d') - datetime.timedelta(days = 1)}/2"
 
         if not os.path.isdir(dir_path):
             print(f"{dir_path} is not exist", file = sys.stderr)

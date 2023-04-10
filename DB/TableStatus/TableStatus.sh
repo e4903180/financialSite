38296f9ed576d14ea9f15e863254b96817a6a213
ueuuop8591 <leo4707@gmail.com>
2023-04-10 17:44:07 +0800

    bug fix

--- DB/TableStatus/TableStatus.sh
+++ DB/TableStatus/TableStatus.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd /home/cosbi/financialSite/DB/TableStatus/TableStatus.py
+cd /home/cosbi/financialSite/DB/TableStatus
 /home/uikai/anaconda3/bin/python /home/cosbi/financialSite/DB/TableStatus/TableStatus.py

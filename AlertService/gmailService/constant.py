54ed54e4d0dd63e90de58ab411ab8a1204b2d240
ueuuop8591 <leo4707@gmail.com>
2023-04-25 15:38:31 +0800

    Log notify service add test twse

--- AlertService/gmailService/constant.py
+++ AlertService/gmailService/constant.py
@@ -2,4 +2,4 @@ SMTP_HOST = "smtp.gmail.com"
 SMTP_PORT = "587"
 
 GMAIL_ACCOUNT = "cosbifinancial@gmail.com"
-GMAIL_AUTH = "sdhkkixtjihovcyt"
+GMAIL_AUTH = "clrwfjjeglqkaewk"
--- LogNotifyService/gmailService/constant.py
+++ LogNotifyService/gmailService/constant.py
@@ -2,6 +2,6 @@ SMTP_HOST = "smtp.gmail.com"
 SMTP_PORT = "587"
 
 GMAIL_ACCOUNT = "cosbifinancial@gmail.com"
-GMAIL_AUTH = "sdhkkixtjihovcyt"
+GMAIL_AUTH = "clrwfjjeglqkaewk"
 
 ADDMIN_EMAIL = "leo4707@gmail.com"

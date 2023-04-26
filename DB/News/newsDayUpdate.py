2f9f94cc60300a60e1363e565be1c3ef7729f949
ueuuop8591 <leo4707@gmail.com>
2023-04-26 15:05:28 +0800

    add exception handle in newsDayUpdate

--- DB/News/newsDayUpdate.py
+++ DB/News/newsDayUpdate.py
@@ -31,7 +31,7 @@ class NewsDayUpdate():
         cursor = db.cursor()
 
         self._ctee = Ctee(options, service, db, cursor)
-        # self._moneydj = MoneyDj(options, service, db, cursor)
+        self._moneydj = MoneyDj(options, service, db, cursor)
         self._money = Money(db, cursor)
         self._statementdog = StatementDog(db, cursor)
     
@@ -44,10 +44,26 @@ class NewsDayUpdate():
             Return :
                 None 
         """
-        # self._moneydj.run()
-        self._money.run()
-        self._statementdog.run()
-        self._ctee.run()
+        try:
+            self._moneydj.run()
+
+        except Exception as e:
+            print(f"MoneyDj error\n{str(e)}", file = sys.stderr)
+
+        try:
+            self._money.run()
+        except Exception as e:
+            print(f"Money error\n{str(e)}", file = sys.stderr)
+
+        try:
+            self._statementdog.run()
+        except Exception as e:
+            print(f"StatementDog error\n{str(e)}", file = sys.stderr)
+
+        try:
+            self._ctee.run()
+        except Exception as e:
+            print(f"Ctee error\n{str(e)}", file = sys.stderr)
 
 if __name__ == "__main__":
     log_notify_service = LogNotifyService()

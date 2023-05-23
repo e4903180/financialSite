cafcf1488a76bc77e62d19a47f8fa3d8ff6e6ec6
ueuuop8591 <leo4707@gmail.com>
2023-05-23 15:01:23 +0800

    realtime price change

--- pythonBackend/backend/api/PythonTool/test.py
+++
@@ -1,7 +0,0 @@
-import twstock
-
-stock_info = twstock.realtime.get("2330")
-
-bid = stock_info["realtime"]["best_bid_price"][-1]
-ask = stock_info["realtime"]["best_ask_price"][-1]
-print(bid, ask)

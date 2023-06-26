db17bb6b1703badad8d844e9ea472cf4d0564b0e
leo4707 <leo4707@gmail.com>
2023-06-26 16:25:43 +0800

    Strategy

---
+++ frontend/build/precache-manifest.5b33f78780ccf196a431b43a1dacdf01.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b0720345d179f4ee772",
+    "url": "/financial/static/css/main.1b335528.chunk.css"
+  },
+  {
+    "revision": "2b0720345d179f4ee772",
+    "url": "/financial/static/js/main.2b072034.chunk.js"
+  },
+  {
+    "revision": "093177959de5bf99fe25",
+    "url": "/financial/static/js/reactPlayerYouTube.09317795.chunk.js"
+  },
+  {
+    "revision": "8175a2bf041426ec2376",
+    "url": "/financial/static/js/reactPlayerSoundCloud.8175a2bf.chunk.js"
+  },
+  {
+    "revision": "d3f2af948f4335e3b7df",
+    "url": "/financial/static/js/reactPlayerVimeo.d3f2af94.chunk.js"
+  },
+  {
+    "revision": "9161631a815315239cf5",
+    "url": "/financial/static/js/reactPlayerFacebook.9161631a.chunk.js"
+  },
+  {
+    "revision": "f1930ec2ad810c9c1f4b",
+    "url": "/financial/static/js/reactPlayerStreamable.f1930ec2.chunk.js"
+  },
+  {
+    "revision": "0946a3b904876506ccf7",
+    "url": "/financial/static/js/reactPlayerWistia.0946a3b9.chunk.js"
+  },
+  {
+    "revision": "8f8aa0c2d3e9e13badf1",
+    "url": "/financial/static/js/reactPlayerTwitch.8f8aa0c2.chunk.js"
+  },
+  {
+    "revision": "2a63a1cb045181cbe584",
+    "url": "/financial/static/js/reactPlayerDailyMotion.2a63a1cb.chunk.js"
+  },
+  {
+    "revision": "eccd5e9d781bc32e54ca",
+    "url": "/financial/static/js/reactPlayerMixcloud.eccd5e9d.chunk.js"
+  },
+  {
+    "revision": "eb8955f6dca7fb089d22",
+    "url": "/financial/static/js/reactPlayerVidyard.eb8955f6.chunk.js"
+  },
+  {
+    "revision": "6e7fb41adaf0b2f341bd",
+    "url": "/financial/static/js/reactPlayerKaltura.6e7fb41a.chunk.js"
+  },
+  {
+    "revision": "4da5e0cab7306217f6e2",
+    "url": "/financial/static/js/reactPlayerFilePlayer.4da5e0ca.chunk.js"
+  },
+  {
+    "revision": "27e0e0dfad9127927c75",
+    "url": "/financial/static/js/reactPlayerPreview.27e0e0df.chunk.js"
+  },
+  {
+    "revision": "1edc7581fb51fda0dbd0",
+    "url": "/financial/static/css/14.e3f1ca46.chunk.css"
+  },
+  {
+    "revision": "1edc7581fb51fda0dbd0",
+    "url": "/financial/static/js/14.1edc7581.chunk.js"
+  },
+  {
+    "revision": "9314d0ea850855278852",
+    "url": "/financial/static/js/runtime~main.9314d0ea.js"
+  },
+  {
+    "revision": "06f64de219a26513be9a8bfb4fbdf8fc",
+    "url": "/financial/index.html"
+  }
+];
